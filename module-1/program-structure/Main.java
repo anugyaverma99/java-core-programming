2bb3beb32d8792882d171c2cbdd96a3659f50df2
anugyaverma <anugyaverma8@gmail.com>
2025-12-25 19:28:42 +0530

    Basic program structure and execution steps

---
+++ module-1/program-structure/Main.java
@@ -0,0 +1,14 @@
+class Main{
+	public static void main(){
+		System.out.println("hello");
+	}
+}
+// compilation syntax- javac FileName.extension
+//execution syntax-- java ClassName
+ // to execute right click on program-structure - open folder-click on path type cmd -enter then compilation and execution commands
+// WHY SOFTWARE?-technology developement (two perspective-dev. and testing)
+// software developement and testing workflow
+// (software company->managing dir.->project manager->team leader-> software developement and testing)
+
+// software application- two layers-FRONTEND LAYER and BACKEND LAYER
+//
