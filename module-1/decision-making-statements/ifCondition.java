6761130bd2f479692f679ff9aff142da8704a7e0
anugyaverma <anugyaverma8@gmail.com>
2025-12-26 23:21:39 +0530

    practiced ternary operator and conditional if statement

---
+++ module-1/decision-making-statements/ifCondition.java
@@ -0,0 +1,15 @@
+public class ifCondition{
+	public static void main(String[] args){
+		int a=10;
+		//TERNARY OPERATOR
+		// String ans=a%2==0?"even number":"odd number";
+
+         // IF STATEMENT
+		if(a%2==0){
+			System.out.println("even number");
+		}
+		if(a%2!=0){
+			System.out.println("odd number");
+		}
+	}
+}
